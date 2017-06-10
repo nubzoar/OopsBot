8ea28cfdab11496c603cbbdaa8947ead643a934c
Devon Thompson <nubzoar@gmail.com>
2017-06-10 12:33:08 -0700

    Working ping/pong example inside expandable command list.

--- bot.js
+++ bot.js
@@ -4,25 +4,42 @@ const discord = require('discord.js');
 // create an instance of a Discord Client, and call it bot
 const bot = new discord.Client();
 
-// get the token for the bot via user input
+bot.on('ready', () => {
+    console.log('I am ready!');
+});
+
+bot.on('message', message => {
+    
+    // So the bot doesn't respond to itself or other bots.
+    if (message.author.bot) return;
+
+    // Check for the command trigger '~'
+    if (message.content.substring(0, 1) === '~') {
+
+        console.log('Trigger spotted!')
+
+        console.log('Here is what I see is the command: ' + message.content.match(/[~](\w+)/)[1])
+
+        if (!cmds[message.content.match(/[~](\w+)/)[1]](message)) {
+            console.log('Command executed but did not return true.');
+        }
+    }
+});
+
+const cmds = {
+
+    ping: function(message) {
+        message.reply('pong');
+        return true;
+    }
+}
+
+// get the token for the bot via user input then login
 const readline = require('readline');
 const interface = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-rl.question("Please enter your bot's token to start: ", function(answer) {
-    const token = answer;
+interface.question("Please enter your bot's token to start: ", function(answer) {
+    bot.login(answer);
 })
-
-bot.on('ready', () => {
-  console.log('I am ready!');
-});
-
-bot.on('message', message => {
-  if (message.content === 'ping') {
-	console.log(message.author);
-    message.channel.sendMessage('pong');
-  }
-});
-
-bot.login(token);
